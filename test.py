90edbcb83e4381f661706f6b29171f871653ff88
ccnode <578753770@qq.com>
2020-02-02 18:13:56 +0800

    test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 size = 2
 page = 3
 
-print("select q_name,is_success,q_date from goods_query_log limit "+str((page-1)*size)+","+str(size))
+print("select q_name,is_success,_date from goods_query_lo limit "+str((page-1)*size)+","+str(size))
